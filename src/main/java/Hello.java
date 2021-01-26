c589439f27f6d394125fafdbe3805420587ab603
Karlygash88 <karlygash.ospan@gmail.com>
2021-01-26 12:40:58 -0800

    movie2 branch watching on the phone before going back to living room

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("Hi");
         // movie on the phone for 10 min
         // movie on the phone for 20 min
+        // another final 10 min before going back to living room
     }
 }
